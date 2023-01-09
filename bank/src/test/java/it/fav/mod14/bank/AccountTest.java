f0b3d6817a950501fd0eb9b23cd2cbf662c44d5b
Davide Rossi <daviderossi@unibo.it>
2023-01-09 22:24:18 +0100

    Moved literals to constants

--- bank/src/test/java/it/fav/mod14/bank/AccountTest.java
+++ bank/src/test/java/it/fav/mod14/bank/AccountTest.java
@@ -5,33 +5,42 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class AccountTest {
+	private static final double EPSILON = 1E-7;
 	
 	@Test
 	void testSuccessfulMakeTransfer() {
+		final String ACCOUNT = "XYZ123";
+		final double BALANCE = 100.0;
+		final double AMOUNT = 50.0;
+		final double BALANCE_LEFT = 50.0;
 		Account account = Account.builder()
 				.setOwner("Superpippo")
 				.setPhoneNumber("1234")
 				.setEmail("super@pippo.com")
-				.setBalance(100.0).build();
+				.setBalance(BALANCE).build();
 
-		assertTrue(account.makeTransfer("XYZ123", 50.0));
-		assertEquals(50.0, account.getBalance(), 1E-7);
+		assertTrue(account.makeTransfer(ACCOUNT, AMOUNT));
+		assertEquals(BALANCE_LEFT, account.getBalance(), EPSILON);
 	}
 
 	@Test
 	void testInsufficientFundsMakeTransfer() {
+		final String ACCOUNT = "XYZ123";
+		final double BALANCE = 100.0;
+		final double AMOUNT = 200.0;
 		Account account = Account.builder()
 				.setOwner("Superpippo")
 				.setPhoneNumber("1234")
 				.setEmail("super@pippo.com")
-				.setBalance(100.0).build();
+				.setBalance(BALANCE).build();
 
-		assertFalse(account.makeTransfer("XYZ123", 200.0));
-		assertEquals(100.0, account.getBalance(), 1E-7);
+		assertFalse(account.makeTransfer(ACCOUNT, AMOUNT));
+		assertEquals(100.0, account.getBalance(), EPSILON);
 	}
 	
 	@Test
 	void testAroundBalanceMakeTransfer() {
+		final String ACCOUNT = "XYZ123";
 		final double AMOUNT = 100.0;
 		final double DELTA = 0.1;
 		Account account = Account.builder()
@@ -40,14 +49,14 @@ class AccountTest {
 				.setEmail("super@pippo.com")
 				.setBalance(AMOUNT).build();
 
-		assertFalse(account.makeTransfer("XYZ123", AMOUNT+DELTA));
-		assertEquals(AMOUNT, account.getBalance(), 1E-7);
+		assertFalse(account.makeTransfer(ACCOUNT, AMOUNT+DELTA));
+		assertEquals(AMOUNT, account.getBalance(), EPSILON);
 
-		assertTrue(account.makeTransfer("XYZ123", AMOUNT-DELTA));
-		assertEquals(DELTA, account.getBalance(), 1E-7);
+		assertTrue(account.makeTransfer(ACCOUNT, AMOUNT-DELTA));
+		assertEquals(DELTA, account.getBalance(), EPSILON);
 
 		account.setBalance(AMOUNT);
-		assertTrue(account.makeTransfer("XYZ123", AMOUNT));
-		assertEquals(0.0, account.getBalance(), 1E-7);
+		assertTrue(account.makeTransfer(ACCOUNT, AMOUNT));
+		assertEquals(0.0, account.getBalance(), EPSILON);
 	}
 }
